70bba306177526198e7c6e61be2672b9f84cc170
Tarun Chawla <65712331+Tarun193@users.noreply.github.com>
2023-05-29 16:15:28 -0400

    added scroll to top products, mission and Completed home page

--- Frontend/src/Components/Products/TopProducts.jsx
+++ Frontend/src/Components/Products/TopProducts.jsx
@@ -1,6 +1,29 @@
 // important imports
 import ProductCard from "./ProductCard";
+import {
+  BsFillArrowLeftSquareFill,
+  BsFillArrowRightSquareFill,
+} from "react-icons/bs";
+import { useRef } from "react";
+
 const TopProducts = () => {
+  // To handle the reference of the section which we have to scroll
+  const productSection = useRef();
+
+  // Function for handling the scrolling logic
+  const handleScroll = (dir) => {
+    const section = productSection.current;
+    const scrollAmount =
+      dir === "left"
+        ? section.scrollLeft - (section.clientWidth + 10)
+        : section.scrollLeft + (section.clientWidth + 10);
+
+    section.scrollTo({
+      left: scrollAmount,
+      behavior: "smooth",
+    });
+  };
+
   const products = [
     {
       id: 1,
@@ -64,15 +87,30 @@ const TopProducts = () => {
     },
   ];
   return (
-    <section className="w-full sm:w-[80%] mx-auto font-semibold mt-6 mb-4 px-2">
+    <section className="w-full sm:w-[80%] mx-auto font-semibold mt-6 mb-4 px-2 relative">
       <h2 className="text-xl sm:text-2xl md:text-3xl text-center sm:text-left my-2">
         Top Products
       </h2>
-      <section className="flex flex-nowrap gap-4 overflow-y-auto px-6 py-3 no-scrollbar">
+      <button
+        className="hidden md:block hover:opacity-100 opacity-50 hover:scale-110 absolute top-[50%] z-10 left-[-1rem]"
+        onClick={() => handleScroll("left")}
+      >
+        <BsFillArrowLeftSquareFill size={20} className="" />
+      </button>
+      <section
+        className="flex flex-nowrap gap-4 overflow-y-auto px-1 py-3 no-scrollbar"
+        ref={productSection}
+      >
         {products.map((product) => (
           <ProductCard key={product.id} product={product} />
         ))}
       </section>
+      <button
+        className="hidden md:block hover:opacity-100 opacity-50 hover:scale-110 absolute top-[50%] right-[-1rem]"
+        onClick={() => handleScroll("right")}
+      >
+        <BsFillArrowRightSquareFill size={20} className="" />
+      </button>
     </section>
   );
 };
