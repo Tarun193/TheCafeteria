59421318cd53f57bbb53fb01900c871a07a9b77a
Tarun Chawla <65712331+Tarun193@users.noreply.github.com>
2023-06-07 21:57:43 -0400

    Fixed some bugs and added admin authorization for admin api calls

--- Backend/TheCafeteria/TheCafeteriaAPI/views.py
+++ Backend/TheCafeteria/TheCafeteriaAPI/views.py
@@ -17,36 +17,41 @@ def GetProducts(request, id=None):
         return Response(SerializedProducts.data)
 
     elif request.method == "POST":
+        if not request.user.is_authenticated or not request.user.is_staff:
+            return Response("Unauthorized", status=401)
         product = ProductSerializer(data=request.data)
-        print(request.data)
         try:
             if product.is_valid():
                 product = product.save()
-                return Response({"message": "Success"}, status=200)
+                return Response(ProductSerializer(product).data, status=200)
             else:
                 return Response({"message": "Invalid Form"}, status=400)
         except Exception:
             return Response({"message": "server error"}, status=500)
 
 
-@api_view(["PUT"])
+@api_view(["PUT", "DELETE"])
 @permission_classes([IsAdminUser])
 def product(request, id=None):
     if request.method == "PUT":
         product = ProductSerializer(
             data=request.data, instance=Product.objects.get(pk=id)
         )
-        print(request.data)
         try:
             if product.is_valid():
                 product = product.save()
-                return Response({"message": "Success"}, status=200)
+
+                return Response(ProductSerializer(product).data, status=200)
             else:
                 print("Error", product.errors)
                 return Response({"message": "Invalid Form"}, status=400)
         except Exception as e:
             print(e)
             return Response({"message": "server error"}, status=500)
+    if request.method == "DELETE":
+        product = Product.objects.get(pk=id)
+        product.delete()
+        return Response(id, status=200)
 
 
 @api_view(["GET"])
